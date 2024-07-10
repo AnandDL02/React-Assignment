25b1ca3202c9500f66a617cde14c28590409fa96
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-07-10 16:13:54 +0530

    commite

---
+++ react-bootstrap-project/project/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Componets/Nav';
+
+function App() {
+  return (
+    <>
+    <Nav/>
+
+    </>
+  );
+}
+
+export default App;
